a90f437f16662908b66b03e9c6ed646b0015d3c7
norahalq <84841948+norahalq@users.noreply.github.com>
2021-08-25 22:40:45 +0300

    Add files via upload
    I used  Html && CSS && js

---
+++ Home_page/front.js
@@ -0,0 +1,13 @@
+
+
+
+// const OperationsNumber=function(num1,num2)
+// {   num1+num2
+//     return 
+//     num1-num2
+//     num1*num2
+//     num1/num2
+//     return num1,num2
+// }
+
+// module.exports=OperationsNumber
